26c5e9ee4ca584e4c56001969a4d0a9a9422fa65
Nicholas Angelopoulos <nicholasangel1357@gmail.com>
2025-11-20 16:19:06 -0500

    porfolio component and styling, header icon fix

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import '../style.css';
 
 const Portfolio = () => {
-  return <h1>Portfolio</h1>;
+  return (
+    <div className='portfolio section'>
+      <h1 className='portfolio-title'>My Work</h1>
+      <div className='pages'></div>
+      <nav className='nav-container'>
+        <ul className='nav-reel'>
+          <li className='reel end'>
+            <span className='arrow'>{'<'}</span>
+          </li>
+          <li className='reel'>1</li>
+          <li className='reel'>2</li>
+          <li className='reel'>3</li>
+          <li className='reel'>4</li>
+          <li className='reel end'>
+            <span className='arrow'>{'>'}</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Portfolio;
